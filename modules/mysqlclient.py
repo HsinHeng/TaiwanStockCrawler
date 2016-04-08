90122d12eb2bacb11b9f8f762500e586db574e2c
Daz <daz.lee1987@gmail.com>
2016-04-08 12:13:57 +0800

    reformat python files

--- modules/mysqlclient.py
+++ modules/mysqlclient.py
@@ -6,51 +6,55 @@ from modules.logger import Logger
 
 logger, _ = Logger.get_instance()
 
+
 class MySQLClient(object):
     def __init__(self, user, password, host, dbname):
-	conn_str = 'mysql://{0}:{1}@{2}/{3}?charset=utf8'.format(user, password, host, dbname)
-	self.engine = create_engine(conn_str)
+        conn_str = 'mysql://{0}:{1}@{2}/{3}?charset=utf8'.format(
+            user, password, host, dbname)
+        self.engine = create_engine(conn_str)
 
     def create_tables(self):
-	metadata.create_all(self.engine)
+        metadata.create_all(self.engine)
 
     def commit(self, stocks):
-	conn = self.engine.connect()
+        conn = self.engine.connect()
 
-	for number, stock in stocks.items():
-	    if not stock.get('record_time'):
-		print stock
-		continue
+        for number, stock in stocks.items():
+            if not stock.get('record_time'):
+                continue
 
-	    stmt = select([latest_stock_info]).where(latest_stock_info.c.number == number)
-    
-	    if conn.execute(stmt).fetchone():
-	        stmt = latest_stock_info.update()\
-		    .where(latest_stock_info.c.number == number)\
-		    .values(latest_price=stock.get('latest_price'),\
-			highest_price=stock.get('highest_price'),\
-			lowest_price=stock.get('lowest_price'),\
-			opening_price=stock.get('opening_price'),\
-			limit_up=stock.get('limit_up'),\
-			limit_down=stock.get('limit_down'),\
-			yesterday_price=stock.get('yesterday_price'),\
-			temporal_volume=stock.get('temporal_volume'),\
-			volume=stock.get('volume'),\
-			top5_sold_prices=stock.get('top5_sold_prices'),\
-			top5_sold_count=stock.get('top5_sold_count'),\
-			top5_buy_prices=stock.get('top5_buy_prices'),\
-			top5_buy_count=stock.get('top5_buy_count'),\
-			record_time=stock.get('record_time'),\
-			updated_at=stock.get('updated_at'))
-		try:
-	    	    conn.execute(stmt)
-		except Exception as e:
-		    logger.error('Failed to update stock info: %s, %s', e, stock)
-		
-	    else:
-		try:
-		    conn.execute(latest_stock_info.insert(), [stock])
-		except Exception as e:
-                    logger.error('Failed to insert stock info: %s, %s', e, stock)
+            stmt = select([latest_stock_info]).where(latest_stock_info.c.number
+                                                     == number)
 
-	conn.close()
+            if conn.execute(stmt).fetchone():
+                stmt = latest_stock_info.update()\
+             .where(latest_stock_info.c.number == number)\
+             .values(latest_price=stock.get('latest_price'),\
+          highest_price=stock.get('highest_price'),\
+          lowest_price=stock.get('lowest_price'),\
+          opening_price=stock.get('opening_price'),\
+          limit_up=stock.get('limit_up'),\
+          limit_down=stock.get('limit_down'),\
+          yesterday_price=stock.get('yesterday_price'),\
+          temporal_volume=stock.get('temporal_volume'),\
+          volume=stock.get('volume'),\
+          top5_sold_prices=stock.get('top5_sold_prices'),\
+          top5_sold_count=stock.get('top5_sold_count'),\
+          top5_buy_prices=stock.get('top5_buy_prices'),\
+          top5_buy_count=stock.get('top5_buy_count'),\
+          record_time=stock.get('record_time'),\
+          updated_at=stock.get('updated_at'))
+                try:
+                    conn.execute(stmt)
+                except Exception as e:
+                    logger.error('Failed to update stock info: %s, %s', e,
+                                 stock)
+
+            else:
+                try:
+                    conn.execute(latest_stock_info.insert(), [stock])
+                except Exception as e:
+                    logger.error('Failed to insert stock info: %s, %s', e,
+                                 stock)
+
+        conn.close()
--- modules/reader.py
+++ modules/reader.py
@@ -3,6 +3,7 @@ import csv
 
 CONF_FILE_PATH = 'config/stock.conf'
 
+
 def getConfig():
     data = dict()
     config = ConfigParser.ConfigParser()
@@ -12,37 +13,34 @@ def getConfig():
         conf = {
             'url': config.get('TW Stock', 'url'),
             'file': config.get('TW Stock', 'file'),
-	    'interval' : int(config.get('TW Stock', 'interval')),
-	    'user': config.get('MySQL', 'user'),
-	    'password': config.get('MySQL', 'password'),
-	    'host': config.get('MySQL', 'host'),
-	    'dbname': config.get('MySQL', 'dbname'),
+            'interval': int(config.get('TW Stock', 'interval')),
+            'user': config.get('MySQL', 'user'),
+            'password': config.get('MySQL', 'password'),
+            'host': config.get('MySQL', 'host'),
+            'dbname': config.get('MySQL', 'dbname'),
         }
     except Exception as e:
         return False, conf
     else:
-	return True, conf
+        return True, conf
+
 
 def getStockList(path):
     stocks = list()
 
     with open(path, 'rb') as csvfile:
-	reader = csv.reader(csvfile)
-	reader.next()
-	reader.next()
+        reader = csv.reader(csvfile)
+        reader.next()
+        reader.next()
         reader.next()
 
-	for row in reader:
+        for row in reader:
 
-	    if not row:
-		continue
+            if not row:
+                continue
 
-	    stocks.append('tse_{}.tw'.format(row[0].strip()))
- 
+            stocks.append('tse_{}.tw'.format(row[0].strip()))
+
     #stocks = ['otc_o00.tw']
     #stocks = ['tse_0050.tw', 'tse_0051.tw']
     return stocks
-	
-	
-	
-
